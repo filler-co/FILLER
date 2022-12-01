1bc5411d44f30ce0523d26e8ebf9d76be4a6eeea
Kyle Martinelli <kylelmartinelli@gmail.com>
2022-11-30 16:19:08 -0800

    configured webpack, babel, and fixed previous document bug,

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const SRC_DIR = path.join(__dirname, '/client/src');
+const SRC_DIR = path.join(__dirname, '/client');
 const DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
