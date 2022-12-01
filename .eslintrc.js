1c0a3f32020a71c697888f7895203893d88bd179
Kyle Martinelli <kylelmartinelli@gmail.com>
2022-12-01 09:20:10 -0800

    completed repo setup

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ['./node_modules/eslint-config-airbnb/index.js', 'airbnb', 'airbnb/hooks'],
+  env: {
+    browser: true,
+  },
+};
---
+++ client/index.jsx
@@ -0,0 +1,17 @@
+import { useState, React } from 'react';
+import ReactDOM from 'react-dom';
+
+export default function App() {
+  // const [test, setTest] = useState('world');
+  // setTest('kyle');
+
+  return (
+
+    <div>
+      Hello
+    </div>
+
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
