a9407a05e4507a0ab690b4fc78f0c0fb620f69ae
Kyle Martinelli <kylelmartinelli@gmail.com>
2022-12-01 23:22:07 -0800

    linter

--- client/src/Components/Reviews/Reviews.jsx
+++ client/src/Components/Reviews/Reviews.jsx
@@ -1,14 +1,11 @@
 import { React, useState, useEffect } from 'react';
 import ReviewItem from './ReviewItem.jsx';
 import axios from "axios";
-import token from '/Users/kylemartinelli/HackReactor/FEC/config.js'
+import token from '../../../../config.js';
 
 
 export default function Reviews({ renderedProduct }) {
-
   const [reviewList, setReviewList] = useState([]);
-
-
 
   // state fro body
 
@@ -16,17 +13,18 @@ export default function Reviews({ renderedProduct }) {
 
   const getProductReview = () => {
     axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews?product_id=40344', { headers: { Authorization: token.TOKEN } })
-      .then((data) => { setReviewList(data.data.results); console.log('data', data.data.results)})
+      .then((data) => { setReviewList(data.data.results); console.log('data', data.data.results); })
       .catch((err) => console.error(err));
   };
 
-  useEffect(() => { getProductReview()}, []);
+  useEffect(() => { getProductReview(); }, []);
 
   return (
     <div className="reviews">
       <h2>Reviews</h2>
 
-      {reviewList.length > 0 ? reviewList.map((review) => <ReviewItem summary={review.summary} />) : <div></div> }
+      {reviewList.length > 0 ? reviewList.map((review) =>
+        <ReviewItem summary={review.summary} />) : <div />}
     </div>
   );
 }
