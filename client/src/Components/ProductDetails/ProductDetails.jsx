7ba3a42ac802f0f14fd0570862bfbc7a9f56bd34
Sai Vemireddy <srisaiteja@gmail.com>
2022-12-03 13:40:20 -0800

    added controllers for get specific product and styles of a specific product as well as added some grid css for product details

--- client/src/Components/ProductDetails/ProductDetails.jsx
+++ client/src/Components/ProductDetails/ProductDetails.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 // import RelatedProducts from './RelatedProducts.jsx';
+import styled from 'styled-components';
+
+const PDContainer = styled.div`
+  display: grid;
+  grid-template-columns: 0.5fr, 0.5fr;
+  grid-template-areas:
+  "product-image product-specs";
+`;
+
+const PDImgContainer = styled.div`
+  grid-area: product-image;
+  background: cornflowerblue;
+  text-align: center;
+`;
+
+const PDSpecsContainer = styled.div`
+  grid-area: product-specs;
+  background: white;
+`;
 
 export default function ProductDetails({renderedProduct, changeRenderedProduct}) {
   return (
-    <div className="">
-      <div className="main">{renderedProduct.name}</div>
-    </div>
-
-  )
-};
-
+    <PDContainer>
+      <PDImgContainer>IMAGE</PDImgContainer>
+      <PDSpecsContainer>
+        <div className="main">PRODUCT NAME: {renderedProduct.name}</div>
+      </PDSpecsContainer>
+    </PDContainer>
+  );
+}
