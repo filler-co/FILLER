97fe5e4526fb27aa1ff8041efbb6354d1c74aecc
Abigail LI <palmigloo@gmail.com>
2022-12-05 13:10:31 -0800

    complete searchbar ui and logic

--- client/src/Components/Questions/Questions.jsx
+++ client/src/Components/Questions/Questions.jsx
@@ -6,6 +6,7 @@ import SearchBar from './SearchBar.jsx';
 import MoreButton from '../Shared/MoreButton.jsx';
 import token from '../../../../config.js';
 import styled, { css } from 'styled-components';
+import { result } from 'lodash';
 
 /* Define style for component*/
 const Container = styled.div`
@@ -53,19 +54,51 @@ const AskQuestionBtn = styled.div`
 `;
 
 export default function Questions({ renderedProduct }) {
+
   const [questions, setQuestions] = useState([]);
+  const [searchResult, setSearchResult] = useState([]);
+
+  const product_id = renderedProduct.id || 40347;
+
   useEffect(() => {
-    // axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp//qa/questions?${renderedProduct.id}`, { headers: { Authorization: token.TOKEN } })
+    // axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp//qa/questions?product_id=${product_id}`, { headers: { Authorization: token.TOKEN } })
     axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/qa/questions?product_id=40347`, { headers: { Authorization: token.TOKEN } })
       .then((response) => {
         console.log('Client side response is : ', response.data);
         setQuestions(response.data.results);
+        setSearchResult(response.data.results);
       })
       .catch((error) => {
         console.log('Client side error is : ', error);
       });
-    // }, [renderedProduct.id]);
-  }, []);
+    }, []);
+  // }, []);
+
+  const searchQuestion = (keyWord) => {
+    console.log('Search questions with keyword : ', keyWord);
+    let results = [];
+    questions.forEach((question, index) => {
+      if (question.question_body.toLowerCase().includes(keyWord)) {
+        console.log('find a match in question');
+        results.push(question);
+      } else {
+        Object.values(question.answers).forEach((answer) => {
+          if (answer.body.toLowerCase().includes(keyWord)) {
+            console.log('find a match in answer')
+            results.push(question);
+          }
+        })
+      }
+    })
+    console.log('search result is : ', results);
+    setSearchResult(results);
+
+  }
+
+  const sortResults = () => {
+    console.log('sorting')
+  }
+
 
   return (
     <Container>
@@ -74,10 +107,10 @@ export default function Questions({ renderedProduct }) {
     </Header>
 
       <Search>
-        <SearchBar />
+        <SearchBar searchQuestion={searchQuestion}/>
       </Search>
       <QAList>
-        {questions.length > 0 ? questions.map((question, index) => <QuestionItem question={question} key={index} />) : 'Still loading'}
+        {searchResult.length > 0 ? searchResult.map((question, index) => <QuestionItem question={question} key={index} />) : 'Still loading'}
       </QAList>
       <MoreQuestionBtn>
         <MoreButton buttonName='MORE ANSWERED QUESTIONS' />
