b0d2fa8c9d8f062712836c6d99e6292ea1bd6f8b
Sai Vemireddy <srisaiteja@gmail.com>
2022-12-05 16:37:33 -0800

    implement preliminary quantity functionality for product details component

--- client/src/Components/ProductDetails/StyleDropdowns.jsx
+++ client/src/Components/ProductDetails/StyleDropdowns.jsx
@@ -21,6 +21,22 @@ const SizeDropdownList = styled.ul`
 const SizeListItem = styled.li`
 `;
 
+const QtyDropdownContainer = styled.div`
+`;
+
+const QtyDropdownHeader = styled.div`
+`;
+
+const QtyDropdownListContainer = styled.div`
+`;
+
+const QtyDropdownList = styled.ul`
+`;
+
+const QtyListItem = styled.li`
+`;
+
+
 
 
 
@@ -28,6 +44,9 @@ export default function StyleDropdowns ({selectedStyle}) {
   const [sizeOpen, setSizeOpen] = React.useState(false);
   const [sizeOption, setSizeOption] = React.useState('Select Size');
   const [sizeList, setSizeList] = React.useState([]);
+  const [qtyOpen, setQtyOpen] = React.useState(false);
+  const [QtyOption, setQtyOption] = React.useState('-');
+  const [qtyList, setQtyList] = React.useState([]);
 
   React.useEffect(() => {
     if (selectedStyle && selectedStyle.skus) {
@@ -35,8 +54,23 @@ export default function StyleDropdowns ({selectedStyle}) {
       .filter((sizeId) => selectedStyle.skus[sizeId].quantity>0)
       .map((sizeId) => {return {sizeId: sizeId, quantity: selectedStyle.skus[sizeId].quantity, size: selectedStyle.skus[sizeId].size}}))
       setSizeOption('Select Size');
+      setSizeOpen(false);
+      setQtyOption('-');
+      setQtyOpen(false);
+      setQtyList([]);
     }
   },  [selectedStyle])
+
+  const loadQty = (quantity) => {
+    var list = [];
+    for (var i = 1; i <= quantity; i++) {
+      list.push(i.toString());
+      if (i === 15) {
+        break;
+      }
+    }
+    setQtyList(list);
+  }
   return (
     <StyleDropdownContainer>
       <SizeDropdownContainer>
@@ -46,12 +80,23 @@ export default function StyleDropdowns ({selectedStyle}) {
         {sizeOpen && (
           <SizeDropdownListContainer>
           <SizeDropdownList>
-            {sizeList.map((sizeObj) => <SizeListItem key={sizeObj.sizeId} onClick={() => {setSizeOption(sizeObj.size); setSizeOpen(!sizeOpen);}}>{sizeObj.size}</SizeListItem>)}
+            {sizeList.map((sizeObj) => <SizeListItem key={sizeObj.sizeId} onClick={() => {setSizeOption(sizeObj.size); setSizeOpen(!sizeOpen); loadQty(sizeObj.quantity); setQtyOption('1')}}>{sizeObj.size}</SizeListItem>)}
           </SizeDropdownList>
         </SizeDropdownListContainer>
         )}
-
       </SizeDropdownContainer>
+      <QtyDropdownContainer>
+        <QtyDropdownHeader onClick={() => setQtyOpen(!qtyOpen)}>
+          {QtyOption}
+        </QtyDropdownHeader>
+        {qtyOpen && (
+          <QtyDropdownListContainer>
+            <QtyDropdownList>
+              {qtyList.map((qty) => <QtyListItem key={qty} onClick={() => {setQtyOption(qty); setQtyOpen(!qtyOpen) }}>{qty}</QtyListItem>)}
+            </QtyDropdownList>
+          </QtyDropdownListContainer>
+        )}
+      </QtyDropdownContainer>
     </StyleDropdownContainer>
   )
 }
